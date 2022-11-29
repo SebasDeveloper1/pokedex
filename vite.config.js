e01edd7f47bcfe8ed6bc4d253227a0d5ffbe07cd
Sebastian Pedroza <sebaspedroza.developer@gmail.com>
2022-11-29 16:11:52 -0500

    :construction: first commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, "src/App"),
+      icons: path.resolve(__dirname, "src/assets/icons"),
+      images: path.resolve(__dirname, "src/assets/images"),
+      videos: path.resolve(__dirname, "src/assets/videos"),
+      components: path.resolve(__dirname, "src/components"),
+      containers: path.resolve(__dirname, "src/containers"),
+      context: path.resolve(__dirname, "src/context"),
+      hooks: path.resolve(__dirname, "src/hooks"),
+      pages: path.resolve(__dirname, "src/pages"),
+      routes: path.resolve(__dirname, "src/routes"),
+      services: path.resolve(__dirname, "src/services"),
+      styles: path.resolve(__dirname, "src/styles"),
+      utils: path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
